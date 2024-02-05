3484f8e0f7ad1642162fc0ee0548af2446711133
crlccc <crl9606@outlook.com>
2024-02-05 20:57:07 +0800

    修改日志

--- src/main/java/com/example/wiki/WikiApplication.java
+++ src/main/java/com/example/wiki/WikiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
 
 }
