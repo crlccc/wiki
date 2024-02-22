22624ab4c33845d29300832c7c7d12272e60b8ca
crlccc <crl9606@outlook.com>
2024-02-22 11:49:46 +0800

    配置spring拦截器

---
+++ src/main/java/com/example/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.wiki.config;
+
+import com.example.wiki.interceptor.LoginInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+
+}
