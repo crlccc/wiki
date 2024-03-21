cec171abe27a6ddf6efa40635adcc7c5873b19ea
crlccc <crl9606@outlook.com>
2024-03-21 21:48:41 +0800

    增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/example/wiki/controller/ControllerExceptionHandler.java
+++ src/main/java/com/example/wiki/controller/ControllerExceptionHandler.java
@@ -1,4 +1,5 @@
 package com.example.wiki.controller;
+import com.example.wiki.exception.BusinessException;
 import com.example.wiki.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +28,25 @@ public class ControllerExceptionHandler {
         LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(BusinessException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.warn("业务异常：{}", e.getCode().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getCode().getDesc());
+        return commonResp;
+    }
+    @ExceptionHandler(value = Exception.class )
+    @ResponseBody
+    public CommonResp validExceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：",e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常！");
         return commonResp;
     }
 }
