268dba5a5585d78de20e94cf5402a3229562e5e5
crlccc <crl9606@outlook.com>
2024-04-06 22:01:57 +0800

    后端增加获取统计数值接口

---
+++ src/main/java/com/example/wiki/config/EbookSnapshotController.java
@@ -0,0 +1,28 @@
+package com.example.wiki.config;
+
+import com.example.wiki.resp.CommonResp;
+import com.example.wiki.resp.StatisticResp;
+import com.example.wiki.service.EbookSnapshotService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic(){
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+}
